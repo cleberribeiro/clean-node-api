e646fbcc33aff92bdebe49de34356ca339d7b4d6
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-06-22 19:45:19 -0300

    feat: ensure Bcrypt sut

--- src/infra/criptografy/bcrypt-adapter.ts
+++ src/infra/criptografy/bcrypt-adapter.ts
@@ -2,8 +2,14 @@ import bcrypt from 'bcrypt'
 import { Encrypter } from './../../data/protocols/encrypter'
 
 export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
   async encrypt (value: string): Promise<string> {
-    const hash = await bcrypt.hash(value, 12)
+    const hash = await bcrypt.hash(value, this.salt)
     return hash
   }
 }
