815ae22e34285c03cdc51585e339f75830821489
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-06-30 14:16:08 -0300

    test: add ExpressRouteAdapter

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -6,6 +6,7 @@ describe('Signup Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
